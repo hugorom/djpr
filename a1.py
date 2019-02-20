84497873b58b2ffb0883cf5571bc50096d9928b3
hugorom <h.rr@live.com.mx>
2019-02-19 18:59:04 -0600

    Primer commit app django

---
+++ a1.py
@@ -0,0 +1,20 @@
+ #print('Hello world')
+# if 12<=1:
+#     print('Okas')
+# elif 10 == 10:
+#     print('blb')
+# else:
+#     print('naaa')
+# def hi():
+#     print('Esta es una')
+#     print('función')
+#
+# # hi()
+# def hi(nombre):
+#     print('hola --> ' + nombre + ' !!!')
+# nombres=['h', 'a', 'j', 'x', 'm']
+# for nombre in nombres:
+#     hi(nombre)
+#     print('Vamporotras')
+for i in range(10,22):
+    print(i)
